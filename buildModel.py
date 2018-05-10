665defad42608a5666df5b465c37489beb95ea14
gtairpline <ibrahimalqattan1999@gmail.com>
2018-05-10 17:57:53 -0600

    Update buildModel.py

--- buildModel.py
+++ buildModel.py
@@ -3,12 +3,12 @@ from pyAudioAnalysis import audioTrainTest as aT
 import os
 from sys import argv
 
-file, dir = argv
+file, dir = argv 
 
-subdirectories = os.listdir(dir)
-subdirectories.pop(0)
+subdirectories = os.listdir(dir) #Finds all dirs inside the dir specified above
+subdirectories.pop(0) #Populates an array
 
-subdirectories = [dir + "/" + subDirName for subDirName in subdirectories]
+subdirectories = [dir + "/" + subDirName for subDirName in subdirectories] #Formatting
 
 print(subdirectories)
-aT.featureAndTrain(subdirectories, 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "svmModel", False)
+aT.featureAndTrain(subdirectories, 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "svmModel", False) #featureAndTrain(listOfDirs, mtWin, mtStep, stWin, stStep, classifierType, modelName, computeBEAT
